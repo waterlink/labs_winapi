6923baa16fabbcaa74f733393b67257e8ec707f0
Alexey Fedorov <waterlink000@gmail.com>
2011-03-12 11:31:38 +0200

    lab_2 initial state

---
+++ lab2/includes.hpp
@@ -0,0 +1,12 @@
+#ifndef __includes_hpp
+#define __includes_hpp
+
+/*					*
+ *	Refactored by Fedorov Alex	*
+ *	   header: includes.hpp		*
+ *					*/
+
+#include <windows.h>
+#include <cstring>
+
+#endif
